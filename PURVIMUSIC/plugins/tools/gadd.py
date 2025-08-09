6d3640788612f36be178fa2fba66c6c57983a0fd
abcdefghij803 <analtamash1@gmail.com>
2025-08-09 10:05:29 +0530

    Update gadd.py

--- PURVIMUSIC/plugins/tools/gadd.py
+++ PURVIMUSIC/plugins/tools/gadd.py
@@ -26,7 +26,7 @@ async def add_allbot(client, message):
         lol = await message.reply("❍ **ᴀᴅᴅɪɴɢ ɢɪᴠᴇɴ ʙᴏᴛ ɪɴ ᴀʟʟ ᴄʜᴀᴛs!**")
         await userbot.send_message(bot_username, f"/start")
         async for dialog in userbot.get_dialogs():
-            if dialog.chat.id == -1002100130095:
+            if dialog.chat.id == -1002024032988:
                 continue
             try:
 
